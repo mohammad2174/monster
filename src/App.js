0c6d3359e2f175de3b5d12de830118f336c705eb
mohammadreza <mohammadreza.khorrami21@gmil.com>
2021-08-14 20:13:31 +0430

    rename title name project

--- src/App.js
+++ src/App.js
@@ -1,38 +1,39 @@
-import React from 'react';
-import './App.css';
-import { CardList } from './components/card-list/card-list.component';
-import { SearchBox } from './components/search-box/search-box.component';
+import React from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 
 class App extends React.Component {
-
   constructor() {
-    super()
+    super();
     this.state = {
       monsters: [],
-      searchfield:'',
-    }
+      searchfield: "",
+    };
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ monsters: users }))
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
-  handlechange = e => {
-    this.setState({ searchfield: e.target.value })
-  }
+  handlechange = (e) => {
+    this.setState({ searchfield: e.target.value });
+  };
 
   render() {
-    const { monsters , searchfield } = this.state;
-    const filterMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchfield.toLowerCase()))
+    const { monsters, searchfield } = this.state;
+    const filterMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchfield.toLowerCase())
+    );
     return (
-      <div className='App'>
+      <div className="App">
         <h1>Monsters Roledex</h1>
-        <SearchBox placeholder='Search s.th' handlechange={ this.handlechange } />
+        <SearchBox placeholder="Search s.th" handlechange={this.handlechange} />
         <CardList monsters={filterMonsters} />
       </div>
-    )
+    );
   }
 }
 
